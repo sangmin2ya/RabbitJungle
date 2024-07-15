ce4c8db910d09305dfe0afe71934531c2fe38552
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-07-15 23:23:04 +0900

    [feat] 적 이동 스크립트 주석 추가
    - 적 이동 스크립트 주석 추가
    - 씬에서 단거리, 장거리 몬스터 프리팹 추가

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -15,9 +15,12 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // player 태그를 향해서 Enemy 오브젝트가 이동
         //GameObject player = GameObject.FindGameObjectWithTag("Player");
         //playerTransform = player.transform;
         //transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed*Time.deltaTime);
         
+        // 
+
     }
 }
