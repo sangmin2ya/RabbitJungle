a269eb34a1a266d1bff79fb0a8aacfde3b2c1c4d
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-07-16 10:39:56 +0900

    [feat] 적 생성 로직 구현
    - 우선은 단거리 몬스터로만 구현
    - 플레이어 위치와 비교하여 정해진 적 개수 만큼 랜덤 생성

---
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject shortEnemy;
+    public GameObject LongEnemy;
+    private Transform playerTransform;
+    private int numberOfEnemy = 10; // 몬스터 생성 개수
+    
+    //맵 상에서 적 생성 반경
+    private float minX = -20.0f;
+    private float maxX = 20.0f;
+    private float minY = -20.0f;
+    private float maxY = 20.0f;
+    private float minDistanceFromPlayer = 3.0f; // 플레이어와 최소 거리
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 스테이지 시작 시 플레이어 위치 찾기
+        GameObject player = GameObject.FindWithTag("Player");
+        playerTransform = player.transform;
+        
+        
+    }
+
+    //임의의 SpawnEnemy 메소드 선언
+    void SpawnEnemys() {
+        int spawnedEnemys = 0;
+        while (spawnedEnemys < numberOfEnemy) {
+            // 몹 스폰 위치 생성
+            Vector2 spawnPosition = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY));
+            //플레이어와의 거리 계산
+            float distanceFromPlayer = Vector2.Distance(spawnPosition, playerTransform.position);
+
+
+            // 플레이어와의 최소 거리보다 길다면 몹 생성. 지금은 단거리 몹만 생성하지만 나중에 중거리 몹과 섞는 로직 짜야할듯
+            if (distanceFromPlayer > minDistanceFromPlayer) {
+                Instantiate(shortEnemy, spawnPosition, Quaternion.identity); // 기본 회전값으로 생성
+                spawnedEnemys++;
+            }
+        }
+    }
+}
