280dc0779381066ca7177a1dd19dc48d35fb25d0
Junho Mun <124476542+juknow@users.noreply.github.com>
2024-07-16 22:02:43 +0900

    [feat] 적 피해에 따라 체력바 피해 구현
    - null값 에러가 나기 때문에 코드 리팩토링 필요
    - 적 hp 피해에 따라 체력바 깎임

--- Assets/Scripts/Enemy/EnemyHeat.cs
+++ Assets/Scripts/Enemy/EnemyHeat.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EnemyHeat : MonoBehaviour
 {
     private int enemyHP = 5;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,28 +18,104 @@ public class EnemyHeat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (enemyHP<=0) {
-            Destroy(gameObject);
-        }
     }
 
     // 충돌 시 
     private void OnCollisionEnter2D(Collision2D collision) 
     {
-        
+
+/* for special weapon
+        switch (DataManager.Instance.Weapon)
+{
+    case nameof(WeaponType.Sword):
+        Destroy(collision.gameObject);
+        enemyHP -= 1;
+        break;
+    
+    case nameof(WeaponType.Axe):
+        Destroy(collision.gameObject);
+        enemyHP -= 2;
+        break;
+    
+    case nameof(WeaponType.Bow):
+        Destroy(collision.gameObject);
+        enemyHP -= 1;
+        break;
+    
+    // 다른 무기 유형들...
+    
+    default:
+        // for no weapon
+        Debug.LogWarning("알 수 없는 무기 유형입니다.");
+        break;
+}
+  */  
+
+
+
+        // different damage according to weapon type
         if (collision.gameObject.CompareTag("Weapon") || collision.gameObject.CompareTag("Skill")) {
 
             if (DataManager.Instance.Weapon == WeaponType.Gun.ToString()) {
+                Debug.Log("총 맞음!");
                 enemyHP -= 1;
             }
             else if (DataManager.Instance.Weapon == WeaponType.Sword.ToString()) {
+                Debug.Log("칼 맞음!");
                 enemyHP -= 3;
             }
             else {
+                Debug.Log("총기 타입 없음!");
                 enemyHP -= 2;
             }
 
         }
+        // when enemy heated, compare enemyHP
+            //GameObject hp1 = transform.Find("HP_1").gameObject;
+
+
+
+            Transform enemyHPTransform = gameObject.transform.Find("EnemyHP");
+            Transform hp2Transform = enemyHPTransform.Find("HP_2");
+            Transform hp3Transform = enemyHPTransform.Find("HP_3");
+            Transform hp4Transform = enemyHPTransform.Find("HP_4");
+            Transform hp5Transform = enemyHPTransform.Find("HP_5");
+            switch(enemyHP) {
+                case <= 0 :
+                Debug.Log("Die!");
+                Destroy(gameObject);
+                break;
+
+                case 1 :
+                Debug.Log("적피 1");
+                Destroy(hp2Transform.gameObject);
+                Destroy(hp3Transform.gameObject);
+                Destroy(hp4Transform.gameObject);
+                Destroy(hp5Transform.gameObject);
+                //Destroy(enemyHPTransform.gameObject.transform.Find("HP_2"));
+                break;
+
+                case 2 :
+                Debug.Log("적피 2");
+                Destroy(hp3Transform.gameObject);
+                Destroy(hp4Transform.gameObject);
+                Destroy(hp5Transform.gameObject);
+                break;
+
+                case 3 :
+                Debug.Log("적피 3");
+                Destroy(hp4Transform.gameObject);
+                Destroy(hp5Transform.gameObject);
+                break;
+
+                case 4 :
+                Debug.Log("적피 4");
+                Destroy(hp5Transform.gameObject);
+                break;
+
+                default :
+                break;
+            }
     }
 }
 
