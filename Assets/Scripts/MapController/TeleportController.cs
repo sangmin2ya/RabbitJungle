2cfca84a25a24c5210691d272aa191a291787f58
sangmin2ya <netboy0524@gmail.com>
2024-07-16 13:34:00 +0900

    [feat] 룸 타입 생성 - 각 맵 방 콜라이더 설정 - 레이어 추가

--- Assets/Scripts/MapController/TeleportController.cs
+++ Assets/Scripts/MapController/TeleportController.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MapController : MonoBehaviour
 {
     //[SerializeField] private GameObject player;
-
     private void Start()
     {
 
@@ -21,7 +21,13 @@ public class MapController : MonoBehaviour
             Debug.Log("지정된 오브젝트와 Door의 충돌 감지!");
             // 문과 연결된 장소로 이동
             transform.position = collision.gameObject.GetComponent<DoorData>().ConnectedDoorPosition;
+        }
 
+        if (collision.gameObject.CompareTag("Portal"))
+        {
+            Debug.Log("지정된 오브젝트와 포탈의 충돌 감지!");
+            // 보스스테이지로 이동
+            SceneManager.LoadScene("BossScene");
         }
     }
 }
